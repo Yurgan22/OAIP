c74a03e97f9897c6daeeac955b3ca577272e99e0
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-09-23 11:05:39 +0300

    Update laabb.c

--- laabb.c
+++ laabb.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
 
+
 int main() {
+
     setlocale(LC_ALL, "Russian");
-    
-    int a[100][100], n, s=0;
-    printf("Введите размерность матрицы N*N. N = ");
-    scanf_s("%d", &n);
+
+    int a[100][100], n, s = 0, error;
+
+    do {
+        printf("Введите размерность матрицы N*N. N = ");
+        error = scanf_s("%d", &n);
+        if (error == 0)
+        {
+            printf("Неверный ввод, потворите попытку.\n");
+            rewind(stdin);
+        }
+    } while (error == 0);
+
     printf("Введите элементы матрицы:\n");
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
         {
-            scanf_s("%d", &a[i][j]);
+            do {
+                error = scanf_s("%d", &a[i][j]);
+                if (error == 0)
+                {
+                    printf("Неверный ввод, потворите попытку. Введите элементы, начиная с a[%d,%d]\n",i+1,j+1);
+                    rewind(stdin);
+                }
+            } while (error == 0);
             if (j == n - 1) s += a[i][j];
         }
-    printf("Cумма элементов последнего столбца равна %2d ", s);
+    printf("Cумма элементов последнего столбца равна - %d \n", s);
     return 0;
-}  
+}

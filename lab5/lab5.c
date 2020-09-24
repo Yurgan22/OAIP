a6ac503301781f4553a4fb4e5eb33ae6b8877358
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-09-25 02:08:40 +0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,23 +1,33 @@
-﻿#include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
 
+void vvod(int* x)
+{
+    while (!(scanf_s("%d", x))) {
+        printf("Данные введены неверно, повторите попытку...\n");
+        rewind(stdin);
+    }
+}
+
+void vvod_array(int* x, int i, int j)
+{
+    while (!(scanf_s("%d", x))) {
+        printf("Данные введены неверно, повторите попытку. Введите элементы, начиная с a[%d][%d]\n", i + 1, j + 1);
+        rewind(stdin);
+    }
+}
+
 int main()
 {
-    int n, m, a[100][100], min, kol = -1, error;
+    int n, m, a[100][100], min, kol = -1;
 
     setlocale(LC_ALL, "Rus");
 
-    do {
-        printf_s("Введите размерность массива N*M = ");
-        error = scanf_s("%d", &n);
-        error = scanf_s("%d", &m);
-        if (error == 0)
-        {
-            printf("Данные введены неверно, повторите попытку...\n");
-            rewind(stdin);
-        }
-    } while (error == 0);
+    printf_s("Введите размерность массива\nN = ");
+    vvod(&n);
+    printf_s("M = ");
+    vvod(&m);
 
     printf_s("Введите элементы массива A[%d][%d]: \n", n, m);
     scanf_s("%d", &a[0][0]);
@@ -27,14 +37,7 @@ int main()
         for (int j = 0; j < m; j++)
         {
             if (i != 0 || j != 0) {
-                do {
-                    error = scanf_s("%d", &a[i][j]);
-                    if (error == 0)
-                    {
-                        printf("Неверный ввод, потворите попытку. Введите элементы, начиная с a[%d,%d]\n", i + 1, j + 1);
-                        rewind(stdin);
-                    }
-                } while (error == 0);
+                vvod_array(&a[i][j], i, j);
                 min = ((a[i][j] < min) ? a[i][j] : min);
             }
         }

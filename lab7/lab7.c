f13108668429bed2039e4c5092d1d12fe80680fe
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-10-21 23:45:00 +0300

    Add files via upload

---
+++ lab7/lab7.c
@@ -0,0 +1,29 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+#include<locale.h>
+#include<malloc.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int n;
+	char* str;
+	printf("Введите количество символов, N = ");
+	scanf_s("%d", &n);
+	getchar();
+	str = (char*)calloc(n + 2, sizeof(char));
+	fgets(str, n + 1, stdin);
+	
+	int j;
+	for (int i = 0; i < n; i++)
+		if (*(str + i) == ':') { j = i; break; }
+
+	while ( (*(str + j + 1) != ':') && (j<n) )
+	{
+		j++;
+		printf("%c", *(str + j));
+	}
+
+	return 0;
+}
